9aa0d8621803729ada8f9061d35371069f9f831a
Changfu.Lin <changfu.lin@xsgames.com>
2024-02-21 15:46:07 +0800

    init

---
+++ src/help.ts
@@ -0,0 +1,21 @@
+/**
+ * 動態設定多層物件的參數
+ * @param data 被設定的物件
+ * @param key 位置
+ * @param value 參數
+ * @returns 
+ */
+export function data_set(data: any, key: string, value: any) {
+    const keys = key.split(".");
+    const firstKey = keys.shift();
+    if (!firstKey) return;
+  
+    if (!(firstKey in data))
+      data[firstKey] = {};
+  
+    if (keys.length < 1) {
+      data[firstKey] = value;
+      return
+    }
+    data_set(data[firstKey], keys.join("."), value);
+  }
