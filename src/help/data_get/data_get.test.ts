44f75f3a779bf41d9acb98e61b9a5db7377232c4
Changfu.Lin <changfu.lin@xsgames.com>
2024-12-25 10:41:29 +0800

    更新 data_get 測試案例名稱以提高可讀性，並調整註解以更清楚地描述函數功能

--- src/help/data_get/data_get.test.ts
+++ src/help/data_get/data_get.test.ts
@@ -1,25 +1,25 @@
 import { data_get } from "./data_get";
 
-test("help.data_get 測試", () => {
+test("help.data_get 基礎", () => {
     const obj = { a: { b: { c: 1 } } };
     expect(data_get(obj, "a.b.c")).toBe(1);
 });
 
-test("help.data_get null 測試", () => {
+test("help.data_get null ", () => {
     expect(data_get(null, "a.b.c")).toBe(null);
 });
 
-test("help.data_get array 測試 1", () => {
+test("help.data_get array 基礎", () => {
     const array = [{ a: 1 }, { a: 2 }, { a: 3 }]
     expect(data_get(array, "a")).toStrictEqual([1, 2, 3]);
 });
 
-test("help.data_get array 測試 2", () => {
+test("help.data_get array 一個參數 key 不一樣", () => {
     const array = [{ a: 1 }, { b: 2 }, { a: 3 }]
     expect(data_get(array, "a")).toStrictEqual([1, null, 3]);
 });
 
-test("help.data_get object 測試 1", () => {
+test("help.data_get object 基礎", () => {
     const array = {
         a: 1,
         b: 2,
@@ -30,7 +30,7 @@ test("help.data_get object 測試 1", () => {
     expect(data_get(array, "c.d")).toStrictEqual([1, 2, 3, 4]);
 });
 
-test("help.data_get object 測試 複合式key", () => {
+test("help.data_get object 複合式key", () => {
     const array = {
         a: 1,
         b: 2,
@@ -42,7 +42,7 @@ test("help.data_get object 測試 複合式key", () => {
 });
 
 
-test("help.data_get object 測試 相似key", () => {
+test("help.data_get object 相似key", () => {
     const array = {
         app: {
             status: 1,
--- src/help/data_get/data_get.ts
+++ src/help/data_get/data_get.ts
@@ -1,14 +1,14 @@
 import { deepCloneWithJson, gettype } from "..";
 
 /**
- * 獲取資料
+ * 獲取資料  
+ * {a:[{b:a},{b:a}]}  
+ * a.b = [a,a]
  * @param data 資料集 {key:value} 
  * @param key 資料對應的位置 app.column.name
  * @param defaultValue 如果取不到值要給予什麼參數，預設 null
- * @returns
- *
- * {a:[{b:a},{b:a}]}
- * a.b = [a,a]
+ * @returns 
+ * @default null
  */
 export function data_get<T = any>(data: { [key: string]: any } | any[] | null | undefined, key: string, defaultValue: any = null,): T {
     switch (gettype(data)) {
